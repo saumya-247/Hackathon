9f7f9d50b1f7fd5e94683eaf9e3d7b9a8d797e72
shravanip419 <shravanip319@gmail.com>
2025-03-01 13:01:39 +0530

    landing page

--- front.js
+++ front.js
@@ -1,26 +1,26 @@
-let slideIndex = 0;
-let slides = document.querySelectorAll('.slide');
-// Function to show the slide at the specified index
-function showSlide(index) {
-    // Reset all slides and dots
-    slides.forEach(slide => slide.style.display = 'none');
-    slides[index].style.display = 'block';
-}
+// let slideIndex = 0;
+// let slides = document.querySelectorAll('.slide');
+// // Function to show the slide at the specified index
+// function showSlide(index) {
+//     // Reset all slides and dots
+//     slides.forEach(slide => slide.style.display = 'none');
+//     slides[index].style.display = 'block';
+// }
 
-// Show the next slide
-function nextSlide() {
-    slideIndex = (slideIndex + 1) % slides.length;
-    showSlide(slideIndex);
-}
+// // Show the next slide
+// function nextSlide() {
+//     slideIndex = (slideIndex + 1) % slides.length;
+//     showSlide(slideIndex);
+// }
 
-// Show the previous slide
-function prevSlide() {
-    slideIndex = (slideIndex - 1 + slides.length) % slides.length;
-    showSlide(slideIndex);
-}
-// Automatically move to the next slide every 3 seconds
-setInterval(() => {
-    nextSlide();
-}, 5000);
+// // Show the previous slide
+// function prevSlide() {
+//     slideIndex = (slideIndex - 1 + slides.length) % slides.length;
+//     showSlide(slideIndex);
+// }
+// // Automatically move to the next slide every 3 seconds
+// setInterval(() => {
+//     nextSlide();
+// }, 5000);
 
-showSlide(slideIndex);
+// showSlide(slideIndex);
