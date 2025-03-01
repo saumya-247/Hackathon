c7e8dd2caed83669c495661b597c80b67e30053a
shravanip419 <shravanip319@gmail.com>
2025-03-01 18:10:47 +0530

    queue

--- front.js
+++ front.js
@@ -1,30 +1,3 @@
-// let slideIndex = 0;
-// let slides = document.querySelectorAll('.slide');
-// // Function to show the slide at the specified index
-// function showSlide(index) {
-//     // Reset all slides and dots
-//     slides.forEach(slide => slide.style.display = 'none');
-//     slides[index].style.display = 'block';
-// }
-
-// // Show the next slide
-// function nextSlide() {
-//     slideIndex = (slideIndex + 1) % slides.length;
-//     showSlide(slideIndex);
-// }
-
-// // Show the previous slide
-// function prevSlide() {
-//     slideIndex = (slideIndex - 1 + slides.length) % slides.length;
-//     showSlide(slideIndex);
-// }
-// // Automatically move to the next slide every 3 seconds
-// setInterval(() => {
-//     nextSlide();
-// }, 5000);
-
-// showSlide(slideIndex);
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth, signInWithPopup, GoogleAuthProvider, OAuthProvider, signInWithEmailAndPassword } 
 from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
